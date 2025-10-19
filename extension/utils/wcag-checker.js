3ad7a81b7a22e869691c6aaee67cba0bd496df80
abhimanyutiwaribot <tiwariabhi2004@gmail.com>
2025-10-19 21:52:31 +0530

    feat: added styles and utils files

---
+++ extension/utils/wcag-checker.js
@@ -0,0 +1,36 @@
+window.wcagChecker = {
+    checkContrast: function(foreground, background) {
+        const ratio = this.getContrastRatio(foreground, background);
+        return {
+            ratio: ratio,
+            passAA: ratio >= 4.5,
+            passAAA: ratio >= 7.0
+        };
+    },
+    
+    getContrastRatio: function(color1, color2) {
+        const l1 = this.getLuminance(color1);
+        const l2 = this.getLuminance(color2);
+        const lighter = Math.max(l1, l2);
+        const darker = Math.min(l1, l2);
+        return (lighter + 0.05) / (darker + 0.05);
+    },
+    
+    getLuminance: function(color) {
+        const rgb = this.hexToRgb(color);
+        const [r, g, b] = [rgb.r, rgb.g, rgb.b].map(val => {
+            val = val / 255;
+            return val <= 0.03928 ? val / 12.92 : Math.pow((val + 0.055) / 1.055, 2.4);
+        });
+        return 0.2126 * r + 0.7152 * g + 0.0722 * b;
+    },
+    
+    hexToRgb: function(hex) {
+        const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? {
+            r: parseInt(result[1], 16),
+            g: parseInt(result[2], 16),
+            b: parseInt(result[3], 16)
+        } : null;
+    }
+};
